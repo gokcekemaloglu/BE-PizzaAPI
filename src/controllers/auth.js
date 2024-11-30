2e8f6a6ca98fda4d4b9dda42f92e520827ccd65c
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-11-30 18:26:25 +0300

    Added authentication functionality: updated auth controller, added authentication middleware, and modified routes for auth and token

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,4 +1,75 @@
 "use strict"
+const passwordEncrypt = require("../helpers/passwordEncrypt")
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
+
+const User = require("../models/user")
+const Token = require("../models/token")
+
+module.exports = {
+    login: async (req, res) => {
+        /*
+            #swagger.tags = ["Authentication"]
+            #swagger.summary = "Login"
+            #swagger.description = 'Login with username (or email) and password for get simpleToken and JWT'
+            #swagger.parameters["body"] = {
+                in: "body",
+                required: true,
+                schema: {
+                    "username": "test",
+                    "password": "aA?123456",
+                }
+            }
+        */
+
+        const {userName, email, password} = req.body
+
+        if(!((userName || email) && password)) {
+            res.errorStatusCode = 401
+            throw new Error("Username/Email and Password required!")
+        }
+
+        const user = await User.findOne({$or: [{userName}, {email}]})
+
+        if (user?.password !== passwordEncrypt(password)) {
+            res.errorStatusCode = 401
+            throw new Error("Username/Email or Password invalid!")
+        }
+
+        if (!user?.isActive) {
+            res.errorStatusCode = 401
+            throw new Error("User is not active!")
+        }
+
+        // Simple Token
+        let tokenData = Token.findOne({userId: user._id })
+        if (!tokenData) {
+            tokenData = await Token.create({
+                userId: user._id,
+                token: passwordEncrypt(user._id + Date.now())
+            })
+        }
+        res.status(200).send({
+            errror: false,
+            message: "Login Successfull",
+            token: tokenData.token,
+            user
+        })
+    },
+    logout: async (req, res) => {
+        /*
+            #swagger.tags = ["Authentication"]
+            #swagger.summary = "simpleToken: Logout"
+            #swagger.description = 'Delete token key.'
+        */
+       const auth = req.headers?.authorization
+       const tokenKey = auth ? auth.split(" ") : null // array
+       const result = await Token.deleteOne({token: tokenKey[1]})
+        res.status(200).send({
+            error: false,
+            message: "Logout Successfull, token deleted",
+            result
+        })
+    },
+}
--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -1,4 +1,16 @@
 "use strict"
 /* -------------------------------------------------------
-    | FULLSTACK TEAM | NODEJS / EXPRESS |
+| FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
+const Token = require("../models/token")
+
+module.exports = async (req, res, next) => {
+
+    const auth = req.headers?.authorization
+    const tokenKey = auth ? auth.split(" ") : null
+    if (tokenKey && tokenKey[0] === "Token") {
+        const tokenData = await Token.findOne({token: tokenKey[1]})
+        req.user = tokenData ? tokenData.userId :false
+    }
+    next()
+}
