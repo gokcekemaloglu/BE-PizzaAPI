4ab07ad770c6292299b8f7a4dccbccac68003e55
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-11-30 18:34:15 +0300

    Added Topping controller, model, and routes with CRUD functionality

--- src/models/topping.js
+++ src/models/topping.js
@@ -3,4 +3,22 @@
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
+const {Schema, model} = mongoose
 /* ------------------------------------------------------- */
+
+const ToppingSchema = new Schema(
+    {
+      name: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+      },
+    },
+    {
+      collection: "toppings",
+      timestamps: true,
+    },
+);
+  
+module.exports = model("Topping", ToppingSchema);
