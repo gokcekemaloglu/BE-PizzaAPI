ecb0a5d34273f6601e45d615c350dfe3f6fadc0a
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-12-01 03:13:00 +0300

    Updated package-lock.json, package.json, and multiple files in src directory (controllers, models, helpers, routes) with new code and dependencies.

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -6,6 +6,7 @@ const passwordEncrypt = require("../helpers/passwordEncrypt")
 
 const User = require("../models/user")
 const Token = require("../models/token")
+const jwt = require("jsonwebtoken")
 
 module.exports = {
     login: async (req, res) => {
@@ -50,10 +51,40 @@ module.exports = {
                 token: passwordEncrypt(user._id + Date.now())
             })
         }
+
+        // JWT
+
+        // const {_id, userName, email, isActive, isAdmin} = user
+        // AccessToken:
+        const accessData = {
+            _id: user._id,
+            userName: user.userName,
+            email: user.email,
+            isActive: user.isActive,
+            isAdmin: user.isAdmin
+        }
+
+        // Convert to jwt
+        // jwt.sign(payload, key, {expireIn: })
+        const accessToken = jwt.sign(accessData, process.env.ACCESS_KEY, {expiresIn: "30min"})
+
+        // RefreshToken:
+        const refreshData = {
+            _id: user._id,
+            password: user.password
+        }
+
+        // Convert to JWT
+        const refreshToken = jwt.sign(refreshData, process.env.REFRESH_KEY, {expiresIn: "1d"})
+
         res.status(200).send({
             errror: false,
             message: "Login Successfull",
             token: tokenData.token,
+            bearer: {
+                access: accessToken,
+                refresh: refreshToken
+            },
             user
         })
     },
